30508d8d488362c78ec64430fac2205561120470
Kang <gongan1556@naver.com>
2023-02-06 17:21:24 +0900

    다국어 설정, ControllerAdvice설정, 예외처리

--- src/main/java/kr/co/coon/mvc/controller/BoardController.java
+++ src/main/java/kr/co/coon/mvc/controller/BoardController.java
@@ -1,10 +1,9 @@
 package kr.co.coon.mvc.controller;
 
-import kr.co.coon.configuration.http.BaseResponse;
-import kr.co.coon.mvc.domain.Board;
-import kr.co.coon.mvc.parameter.BoardParameter;
-import kr.co.coon.mvc.service.BoardService;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,8 +15,12 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
-
-import java.util.List;
+import kr.co.coon.configuration.exception.BaseException;
+import kr.co.coon.configuration.http.BaseResponse;
+import kr.co.coon.configuration.http.BaseResponseCode;
+import kr.co.coon.mvc.domain.Board;
+import kr.co.coon.mvc.parameter.BoardParameter;
+import kr.co.coon.mvc.service.BoardService;
 
 /**
  * 게시판 컨트롤러
@@ -49,20 +52,25 @@ public class BoardController {
      * @return
      */
     @GetMapping("/{boardSeq}")
-    @ApiOperation(value = "상세 조회", notes = "게시물 번호에 해당하는 상세 정보를 조회할 수 있습니다.")
-    @ApiImplicitParams({
-    	@ApiImplicitParam(name="boardSeq", value="게시물 번호", example="1")
-    })
-    public BaseResponse<Board> get(@PathVariable int boardSeq) {
-
-        return new BaseResponse<Board> (boardService.get(boardSeq));
-    }
+	@ApiOperation(value = "상세 조회", notes = "게시물 번호에 해당하는 상세 정보를 조회할 수 있습니다.")
+	@ApiImplicitParams({
+		@ApiImplicitParam(name = "boardSeq", value = "게시물 번호", example = "1")
+	})
+	public BaseResponse<Board> get(@PathVariable int boardSeq) {
+		Board board = boardService.get(boardSeq);
+		// null 처리
+		if (board == null) {
+			throw new BaseException(BaseResponseCode.DATA_IS_NULL, new String[] { "게시물" });
+		}
+		return new BaseResponse<Board>(board);
+	}
 
     /**
      * 등록 수정 처리.
-     * @param board
+     * @param parameter
      */
-    @PutMapping
+    @SuppressWarnings("deprecation") // 임시
+	@PutMapping
     @ApiOperation(value = "등록 / 수정 처리", notes = "신규 게시물 저장 및 기존 게시물 업데이트가 가능합니다.")
     @ApiImplicitParams({
     	@ApiImplicitParam(name="boardSeq", value="게시물 번호", example="1"),
@@ -70,8 +78,16 @@ public class BoardController {
     	@ApiImplicitParam(name="contents", value="내용", example="내용"),
     })
     public BaseResponse<Integer> save(BoardParameter parameter){
-        boardService.save(parameter);
-        return new BaseResponse<Integer>(parameter.getBoardSeq());
+		// 제목 필수 체크
+		if (StringUtils.isEmpty(parameter.getTitle())) {
+			throw new BaseException(BaseResponseCode.VALIDATE_REQUIRED, new String[] { "title", "제목" });
+		}
+		// 내용 필수 체크
+		if (StringUtils.isEmpty(parameter.getContents())) {
+			throw new BaseException(BaseResponseCode.VALIDATE_REQUIRED, new String[] { "contents", "내용" });
+		}
+		boardService.save(parameter);
+		return new BaseResponse<Integer>(parameter.getBoardSeq());
     }
 
     /**
